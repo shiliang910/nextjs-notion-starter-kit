febe3613444e75040b82b634ced844b50c219fe8
shiliang910 <87292631+shiliang910@users.noreply.github.com>
2022-11-10 16:28:32 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Nash-s-diary-of-learning-e-commerce-1f7e401d69494fc0b2b4492abf9cdbf1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Nash's diary of learning e-commerce',
+  domain: 'https://pollen-ballcap-2b3.notion.site/Nash-s-diary-of-learning-e-commerce-1f7e401d69494fc0b2b4492abf9cdbf1',
+  author: 'liang nash',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '@shiliang0194,
+  github: 'shiliang910',
+  linkedin: '',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
